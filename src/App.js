d69e06cda1f76fb19470ade232c9b798c3378818
nisho786 <mushfiq.f2ed@gmail.com>
2020-09-07 12:22:16 +0600

    shop final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-    return ( <
-        div >
-        <
-        Header > < /Header> <
-        Shop > < /Shop> <
-        /div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
 }
 
 export default App;
